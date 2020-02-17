24b0ffdc64dd33f56ae41ec3caf4b18a319e532a
othillo <othillo@othillo.nl>
2020-02-17 11:04:58 +0100

    PHPUnit 8

--- test/MongoDBRepositoryFactoryTest.php
+++ test/MongoDBRepositoryFactoryTest.php
@@ -4,24 +4,21 @@ namespace Broadway\ReadModel\MongoDB;
 
 use Broadway\Serializer\Serializer;
 use MongoDB\Collection;
-use PHPUnit_Framework_TestCase;
+use PHPUnit\Framework\TestCase;
 
 /**
  * @author Robin van der Vleuten <robin@webstronauts.co>
  */
-class MongoDBRepositoryFactoryTest extends PHPUnit_Framework_TestCase
+class MongoDBRepositoryFactoryTest extends TestCase
 {
     /**
      * @test
      */
     public function it_creates_a_mongodb_repository()
     {
-        $collection = $this->getMockBuilder(Collection::class)
-            ->disableOriginalConstructor()
-            ->getMock();
+        $collection = $this->createMock(Collection::class);
 
-        $serializer = $this->getMockBuilder(Serializer::class)
-            ->getMock();
+        $serializer = $this->createMock(Serializer::class);
 
         $repository = new MongoDBRepository($collection, $serializer, '[CLASS]');
         $factory = new MongoDBRepositoryFactory($collection, $serializer);
